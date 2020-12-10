1f9781b87eb38996831063f60bb2f5577a84692e
jpardike <jpardike@gmail.com>
2020-12-09 17:47:43 -0800

    Adds updates

--- exercises/pixart/pixart.js
+++ exercises/pixart/pixart.js
@@ -1,1 +1,59 @@
+const $setColorButton = $('#set-color');
+const $colorInput = $('#color-field');
+const $brush = $('.brush');
+const $body = $('body');
+let colorSelected = '';
 
+console.log($colorInput);
+
+console.log($setColorButton);
+
+$setColorButton.on('click', function(event) {
+    event.preventDefault();
+
+    const inputVal = $colorInput.val();
+
+    console.log(inputVal);
+
+    console.log('button was clicked');
+
+    $brush.css('background-color', inputVal);
+
+    colorSelected = inputVal;
+});
+
+// STEP 2
+
+function createSquares(numOfSquares) {
+    for (let i = 0; i < numOfSquares; i++) {
+        const $square = $('<div />');
+    $square.addClass('square');
+
+    console.log($square);
+
+    $body.append($square);
+    }
+    
+}
+
+createSquares(8000);
+
+// STEP 3
+
+// $body.on('click', '.square', function() {
+//     $(this).css('background-color', 'green');
+// })
+
+// STEP 4
+
+$body.on('click', '.square', function() {
+    
+    $(this).css('background-color', colorSelected);
+})
+
+// STEP 5
+
+$body.on('mouseover', '.square', function() {
+    
+    $(this).css('background-color', colorSelected);
+})
